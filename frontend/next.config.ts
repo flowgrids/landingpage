0809ae9da8aa9cc74ed048efbef891da9a448d2e
Vincent Trötschel <vincent.troetschel@mailbox.org>
2025-07-15 09:40:31 +0200

    [fix] Pipeline: explicitly export: Ditch automatic export

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    output: "export",
+    // Recreate GitHub injection:
+    basePath: "/landingpage",
+    assetPrefix: "/landingpage",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
