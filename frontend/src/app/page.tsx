5e08b279c28a764f989a8a416776e655fc291eab
Vincent Trötschel <vincent.troetschel@mailbox.org>
2025-07-18 17:31:10 +0200

    [clean] Prefer pure CSS bg approach [style] Better hero img in mobile

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,6 +1,5 @@
 import HeroSection from "./rsc/hero-section";
 import TeamSection from "./rsc/team-section";
-import Image from "next/image";
 import flowgridsBG from "#/flowgrids-bg.svg";
 import PartnerSection from "./rsc/partner-section";
 
@@ -8,16 +7,10 @@ export default function HomePage() {
   return (
     <div className="relative">
       {/* Background Image */}
-      <div className="absolute -z-10 h-screen w-full">
-        <Image
-          src={flowgridsBG}
-          alt="Background Pattern"
-          className="object-cover md:object-contain md:object-right"
-          fill
-          sizes="100vw"
-          priority={true}
-        />
-      </div>
+      <div
+        className={`absolute -z-10 h-screen w-full bg-no-repeat bg-cover bg-top md:bg-contain md:bg-right`}
+        style={{ backgroundImage: `url(${flowgridsBG.src})` }}
+      ></div>
 
       <div className="relative backdrop-blur-sm bg-white/20 dark:bg-black/20">
         <HeroSection />
